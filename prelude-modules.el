427b99c72e7c093d2dbec9ec89245e0521980790
Andy L <happy.lisper@gmail.com>
2018-12-18 16:22:37 -0700

    updates

---
+++ prelude-modules.el
@@ -0,0 +1,11 @@
+;;; Uncomment the modules you'd like to use and restart Prelude afterwards
+
+;; Emacs IRC client
+;;(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-c)
+(require 'prelude-clojure)
+(require 'prelude-emacs-lisp)
+(require 'prelude-js)
+(require 'prelude-lisp)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
+(require 'prelude-shell)
